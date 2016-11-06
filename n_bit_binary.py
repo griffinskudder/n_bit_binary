dc73b4c891136c0bac0609135235ec8513a412ca
Griffin Skudder <gskudder@vectorsystems.co.nz>
2016-11-07 07:42:11 +1300

    Added license info to code files.

--- n_bit_binary.py
+++ n_bit_binary.py
@@ -1,8 +1,20 @@
 """
 This module contains custom types for dealing with n-bit binary numbers.
 Author: Griffin Skudder
-Company: Vector Systems Ltd.
-Email: gskudder@vectorsystems.co.nz
+
+Copyright 2016 Griffin Skudder
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 
