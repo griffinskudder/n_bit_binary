f4a1789fd820c4d02d9407d6aaaf51e10aac64bf
griffinskudder-vector <gskudder@vectorsystems.co.nz>
2024-08-17 10:37:01 +1000

    Create n_bit_binary package. Remove redundant code.

--- n_bit_binary/n_bit_binary.py
+++ n_bit_binary/n_bit_binary.py
@@ -2,7 +2,7 @@
 This module contains custom types for dealing with n-bit binary numbers.
 Author: Griffin Skudder
 
-Copyright 2016 Griffin Skudder
+Copyright 2024 Griffin Skudder
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -28,11 +28,15 @@ else:
 class NBitInteger(SupportsInt):
     """
     An n-bit two's complement binary integer type.
-    Use slicing syntax to modify individual _bits. Index 0 is the most significant bit.
-    Slicing with negative numbers is supported. However as the integer has a fixed length I can't see a use-case.
+    Use slicing syntax to modify individual _bits. Index 0 is the most
+    significant bit.
+    Slicing with negative numbers is supported. However, as the integer has a
+    fixed length I can't see a use-case.
     Where a string representation is needed a base-10 number will be given.
-    Changing the integer being represented via properties is also possible. Assign an integer to self.number to do this.
-    The number of bits can be changed via a property. Assign a positive, non-zero integer to self.bits to do this.
+    Changing the integer being represented via properties is also possible.
+    Assign an integer to self.number to do this.
+    The number of bits can be changed via a property. Assign a positive,
+    non-zero integer to self.bits to do this.
     WARNING: Reducing the number of bits may cause data loss.
     Be sure you understand two's complement before you do this.
     For example:
@@ -48,8 +52,9 @@ class NBitInteger(SupportsInt):
     # n can now be represented as 0000000000000110 or 6
     n.bits = 8
     # n can now be represented as 00000110 or 6
-    An NBitInteger cannot have a length of zero. This applies to slicing as well. Therefore the slice [12:6] is invalid
-    and will throw a ValueError. However, a slice of [12:6:-1] is valid and will reverse the bits.
+    An NBitInteger cannot have a length of zero. This applies to slicing as
+    well. Therefore the slice [12:6] is invalid and will throw a ValueError.
+    However, a slice of [12:6:-1] is valid and will reverse the bits.
     The methods append and prepend are available.
     """
 
@@ -60,9 +65,9 @@ class NBitInteger(SupportsInt):
         self._bits = bits
         if self._signed:
             self._max = 2 ** self.bits // 2 - 1
-            self._min = self._max * -1 + 1
+            self._min = self._max * -1
         else:
-            self._max = 2 ** self.bits - 1
+            self._max = 2 ** self.bits
             self._min = 0
         number = int(number)
         if number > self._max:
@@ -93,13 +98,19 @@ class NBitInteger(SupportsInt):
         int(value)
         if value <= 0:
             raise ValueError("Bits must be greater zero.")
-        self._bits = value
         if self._signed:
-            self._max = 2 ** self.bits // 2 - 1
-            self._min = self._max * -1 + 1
+            temp_max = 2 ** value // 2 - 1
+            temp_min = temp_max * -1 + 1
         else:
-            self._max = 2 ** self.bits - 1
-            self._min = 0
+            temp_max = 2 ** value - 1
+            temp_min = 0
+        bit_string = self.bit_string()
+        temp_value = int(bit_string, 2)
+        if temp_value > temp_max or temp_value < temp_min:
+            raise OverflowError
+        self._bits = value
+        self._max = temp_max
+        self._min = temp_min
         self._number = int(self.bit_string(), 2)
 
     @property
@@ -121,78 +132,27 @@ class NBitInteger(SupportsInt):
             raise OverflowError
         self._signed = value
 
-    def _set_bit(self, offset):
+    def _set_bit(self, offset: int):
         """
         Set bit at position offset.
         :param offset: Must be an integer. 0 is LSB.
         :return:
         """
-        int(offset)
-        if offset < 0:
-            offset += self.bits
-        if offset > (self.bits - 1) or offset < 0:
-            raise IndexError
+        offset = int(offset)
         mask = 1 << offset
         self._number |= mask
-        if self._signed:
-            if self._number > self._max:
-                self._number -= 2 ** self.bits
-            elif self._number < self._min:
-                self._number += 2 ** self.bits
-        else:
-            if self._number > self._max:
-                raise OverflowError
-            elif self._number < self._min:
-                raise OverflowError
+        if self._signed and self._number > self._max:
+            self._number -= 2 ** self.bits
 
-    def _clear_bit(self, offset):
+    def _clear_bit(self, offset: int):
         """
         Clear bit at position offset.
         :param offset: Must be an integer. 0 is LSB.
         :return:
         """
-        int(offset)
-        if offset < 0:
-            offset += self.bits
-        if offset > (self.bits - 1) or offset < 0:
-            raise IndexError
+        offset = int(offset)
         mask = ~(1 << offset)
         self._number &= mask
-        if self._signed:
-            if self._number > self._max:
-                self._number -= 2 ** self.bits
-            elif self._number < self._min:
-                self._number += 2 ** self.bits
-        else:
-            if self._number > self._max:
-                raise OverflowError
-            elif self._number < self._min:
-                raise OverflowError
-
-    def append(self, value):
-        """
-        Append a new least-significant bit to the integer.
-        :param value:
-        :return:
-        """
-        self.bits += 1
-        self._number <<= 1
-        if value:
-            self._set_bit(0)
-        else:
-            self._clear_bit(0)
-
-    def prepend(self, value):
-        """
-        Prepend a new most-significant bit to the integer.
-        :param value:
-        :return:
-        """
-        self.bits += 1
-        if value:
-            self._set_bit(self.bits - 1)
-        else:
-            self._clear_bit(self.bits - 1)
 
     def bit_string(self):
         """
@@ -216,10 +176,15 @@ class NBitInteger(SupportsInt):
             raise ValueError
 
         if len(key) < 1:
-            raise ValueError("This slice {key} would return an integer with 0 bits.".format(key=str(key)))
+            raise ValueError(
+                "This slice {key} would return an integer with 0 bits.".format(
+                    key=str(key)))
 
-        if (key.start < 0 and self.bits - key.start < 0) or (key.start > self.bits - 1) or \
-                (key.stop < 0 and self.bits - key.stop < 0) or key.stop > self.bits:
+        if (key.start < 0 and self.bits - key.start < 0) or (
+                key.start > self.bits - 1
+        ) or (
+                key.stop < 0 and self.bits - key.stop < 0
+        ) or key.stop > self.bits:
             raise IndexError
 
         return_value = NBitInteger(0, 1)
@@ -228,7 +193,8 @@ class NBitInteger(SupportsInt):
             if i < 0:
                 i += self.bits
             mask = 1 << self.bits - 1 - i
-            # By performing a bitwise AND operation on this mask and a number we can determine if the bit at offset
+            # By performing a bitwise AND operation on this mask and a number
+            # we can determine if the bit at offset
             # i is True or False
             if first_run:
                 if self._number & mask:
@@ -249,8 +215,11 @@ class NBitInteger(SupportsInt):
         else:
             raise ValueError
 
-        if (key.start < 0 and self.bits - key.start < 0) or (key.start > self.bits - 1) or \
-                (key.stop < 0 and self.bits - key.stop < 0) or key.stop > self.bits:
+        if (key.start < 0 and self.bits - key.start < 0) or (
+                key.start > self.bits - 1
+        ) or (
+                key.stop < 0 and self.bits - key.stop < 0
+        ) or key.stop > self.bits:
             raise IndexError
 
         for i in key:
@@ -292,80 +261,108 @@ class NBitInteger(SupportsInt):
         return self._number == value
 
     def __add__(self, other):
-        return NBitInteger(self.number + other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number + other, signed=self.signed,
+                           bits=self.bits)
 
     def __sub__(self, other):
-        return NBitInteger(self.number - other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number - other, signed=self.signed,
+                           bits=self.bits)
 
     def __mul__(self, other):
-        return NBitInteger(self.number * other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number * other, signed=self.signed,
+                           bits=self.bits)
 
     def __floordiv__(self, other):
-        return NBitInteger(self.number // other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number // other, signed=self.signed,
+                           bits=self.bits)
 
     def __truediv__(self, other):
-        return NBitInteger(self.number / other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number / other, signed=self.signed,
+                           bits=self.bits)
 
     def __mod__(self, other):
-        return NBitInteger(self.number % other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number % other, signed=self.signed,
+                           bits=self.bits)
 
     def __pow__(self, power):
-        return NBitInteger(self.number ** power, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number ** power, signed=self.signed,
+                           bits=self.bits)
 
     def __lshift__(self, other):
-        return NBitInteger(self.number << other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number << other, signed=self.signed,
+                           bits=self.bits)
 
     def __rshift__(self, other):
-        return NBitInteger(self.number >> other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number >> other, signed=self.signed,
+                           bits=self.bits)
 
     def __and__(self, other):
-        return NBitInteger(self.number & other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number & other, signed=self.signed,
+                           bits=self.bits)
 
     def __xor__(self, other):
-        return NBitInteger(self.number ^ other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number ^ other, signed=self.signed,
+                           bits=self.bits)
 
     def __or__(self, other):
-        return NBitInteger(self.number | other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number | other, signed=self.signed,
+                           bits=self.bits)
 
     def __radd__(self, other):
-        return NBitInteger(self.number + other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number + other, signed=self.signed,
+                           bits=self.bits)
 
     def __rsub__(self, other):
-        return NBitInteger(self.number - other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number - other, signed=self.signed,
+                           bits=self.bits)
 
     def __rmul__(self, other):
-        return NBitInteger(self.number * other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number * other, signed=self.signed,
+                           bits=self.bits)
 
     def __rfloordiv__(self, other):
-        return NBitInteger(self.number // other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number // other, signed=self.signed,
+                           bits=self.bits)
 
     def __rtruediv__(self, other):
-        return NBitInteger(self.number / other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number / other, signed=self.signed,
+                           bits=self.bits)
 
     def __rmod__(self, other):
-        return NBitInteger(self.number % other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number % other, signed=self.signed,
+                           bits=self.bits)
 
     def __rpow__(self, power):
-        return NBitInteger(self.number ** power, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number ** power, signed=self.signed,
+                           bits=self.bits)
 
     def __rlshift__(self, other):
-        return NBitInteger(self.number << other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number << other, signed=self.signed,
+                           bits=self.bits)
 
     def __rrshift__(self, other):
-        return NBitInteger(self.number >> other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number >> other, signed=self.signed,
+                           bits=self.bits)
 
     def __rand__(self, other):
-        return NBitInteger(self.number & other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number & other, signed=self.signed,
+                           bits=self.bits)
 
     def __rxor__(self, other):
-        return NBitInteger(self.number ^ other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number ^ other, signed=self.signed,
+                           bits=self.bits)
 
     def __ror__(self, other):
-        return NBitInteger(self.number | other, signed=self.signed, bits=self.bits)
+        return NBitInteger(self.number | other, signed=self.signed,
+                           bits=self.bits)
 
     def __neg__(self):
         if self.signed:
-            return NBitInteger(-self.number, signed=self.signed, bits=self.bits)
+            return NBitInteger(
+                -self.number,
+                signed=self.signed,
+                bits=self.bits
+            )
         else:
             if -self.number < 0:
                 return NBitInteger(-self.number, signed=True, bits=self.bits)
@@ -374,7 +371,11 @@ class NBitInteger(SupportsInt):
         return NBitInteger(+self.number, signed=self.signed, bits=self.bits)
 
     def __abs__(self):
-        return NBitInteger(abs(self.number), signed=self.signed, bits=self.bits)
+        return NBitInteger(
+            abs(self.number),
+            signed=self.signed,
+            bits=self.bits
+        )
 
     def __invert__(self):
         return NBitInteger(~self.number, signed=self.signed, bits=self.bits)
